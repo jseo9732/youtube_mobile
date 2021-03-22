7762cc68f4408fe86227a2252c52dcbda0a044b0
jseo9732 <79249376+jseo9732@users.noreply.github.com>
2021-03-22 19:15:47 +0900

    Change color when like,hate button is clicked
    좋아요, 싫어요 버튼 클릭 시 색 바꾸기

--- main.js
+++ main.js
@@ -5,3 +5,25 @@ moreBtn.addEventListener('click', () => {
   moreBtn.classList.toggle('clicked');
   title.classList.toggle('clamp');
 });
+
+function likebtnColor(self){
+  var target = document.querySelector('#likebtn');
+  if(self.value === 'unlike'){
+    target.style.color = '#045fd4';
+    self.value = 'like';
+  } else{
+      target.style.color = '#9d9d9d';
+      self.value = 'unlike';
+    }
+  }
+
+  function hatebtnColor(self){
+    var target = document.querySelector('#hatebtn');
+    if(self.value === 'unhate'){
+      target.style.color = '#045fd4';
+      self.value = 'hate';
+    } else{
+        target.style.color = '#9d9d9d';
+        self.value = 'unhate';
+      }
+    }
